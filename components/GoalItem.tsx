48d72bdd91e03243ad24923f81cb2ecb18cbf927
Federica Passanante <federica.passanante@sideagroup.com>
2024-05-16 09:50:51 +0200

    added comment

--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Pressable } from "react-native";
 
 export default function GoalItem(this: any, props: any) {
   return (
+    // .bind() is a JS func that pre-conf a function for future use, 1° arg is 'this' keyword, 2° arg is first arg of called funct 
     <Pressable onPress={props.onDeleteItem.bind(this, props.item.id)}>
       <View style={styles.goalItem}>
           <Text style={styles.goalText}>{props.item.text}</Text>
