3e78ae7b419bc0acad16ea1dae56f6cdc9dbf216
Federica Passanante <federica.passanante@sideagroup.com>
2024-05-16 12:03:54 +0200

    added Image

--- components/GoalInput.tsx
+++ components/GoalInput.tsx
@@ -1,4 +1,4 @@
-import {View, TextInput, Button, StyleSheet, Modal} from 'react-native';
+import {View, TextInput, Button, StyleSheet, Modal, Image} from 'react-native';
 import {useState} from 'react';
 export default function GoalInput(props: any) {
   const [enteredText, setEnteredText] = useState('');
@@ -12,11 +12,10 @@ export default function GoalInput(props: any) {
     setEnteredText('');
   };
 
-  const btnColor = '#459AC2';
-
   return (
     <Modal visible={props.isModalVisible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image source={require('../assets/goal.png')} style={styles.img}/>
         <TextInput
           style={styles.textInput}
           placeholder="Aggiungi nuovo obiettivo"
@@ -26,12 +25,12 @@ export default function GoalInput(props: any) {
         <View style={styles.buttonContainer}>
           <Button
             title="Aggiungi obiettivo"
-            color={btnColor}
+            color="#459AC2"
             onPress={addGoalHandler}
           />
           <Button
             title="Chiudi"
-            color={btnColor}
+            color="#D34972"
             onPress={props.onCloseModal}
           />
         </View>
@@ -43,19 +42,27 @@ export default function GoalInput(props: any) {
 const styles = StyleSheet.create({
   inputContainer: {
     justifyContent: 'center',
-    marginHorizontal: 24,
-    // borderBottomWidth: 1,
-    // borderBottomColor: '#cccccc',
+    paddingHorizontal: 24,
     flex: 1,
+    backgroundColor: '#133F53'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
+    borderRadius: 8,
+    backgroundColor: '#fff',
     marginBottom: 8,
-    padding: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16
   },
   buttonContainer: {
     gap: 12,
     marginTop: 18,
   },
+  img: {
+    width: 200,
+    height: 200,
+    marginVertical: 20,
+    marginHorizontal: 'auto'
+  }
 });
